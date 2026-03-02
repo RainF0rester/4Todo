f6f736306b927c596970d8e6f5172f5da99393bc
yulinliu <liuyulin7@outlook.com>
2026-03-02 20:21:22 +1100

    feat: init sql and backend basic files

---
+++ config.py
@@ -0,0 +1,4 @@
+# config.py
+class Config:
+    # SQLAlchemy URL for SQLite file
+    SQLALCHEMY_DATABASE_URL = "sqlite:///taskmaster.db"
---
+++ schema.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS tasks (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  task_title TEXT NOT NULL,
+  task_due TEXT,                 -- store as ISO date string: 'YYYY-MM-DD'
+  task_description TEXT,
+  task_level INTEGER NOT NULL DEFAULT 0,
+  is_finished INTEGER NOT NULL DEFAULT 0  -- 0=false, 1=true
+);
