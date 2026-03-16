4fb08a1e896888013eff1490a27d976bf0c67e0e
yulinliu <liuyulin7@outlook.com>
2026-03-17 09:28:00 +1100

    feat: init model for users

---
+++ modules/user/models.py
@@ -0,0 +1,27 @@
+from sqlalchemy import Integer, String, Text, Date, DateTime, func
+from sqlalchemy.orm import Mapped, mapped_column
+from db import Base
+
+class User(Base):
+    __tablename__ = 'user'
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+
+    username: Mapped[str] = mapped_column(String(16), nullable=False)
+    email: Mapped[str] = mapped_column(String(64), nullable=False)
+    password_hash: Mapped[str] = mapped_column(String(32), nullable=False)
+
+    is_deleted: Mapped[int] = mapped_column(Integer, nullable=False, default=0)
+    created_at: Mapped[str] = mapped_column(DateTime(timezone=True), server_default=func.now(), nullable=False)
+    update_at: Mapped[str] = mapped_column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now(), nullable=False)
+
+    def to_dict(self) -> dict:
+        return {
+            'id': self.id,
+            'username': self.username,
+            'email': self.email,
+            'password_hash': self.password_hash,
+            'is_deleted': self.is_deleted,
+            'created_at': self.created_at,
+            'update_at': self.update_at,
+        }
