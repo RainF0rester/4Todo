359c99d90c31d4890b4783969d02358bc7d02f65
yulinliu <liuyulin7@outlook.com>
2026-04-16 13:57:37 +1000

    feat: add more files for cd

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+mkdir -p ./data
+if [ ! -f ./data/taskmaster.db ]; then
+    touch ./data/taskmaster.db
+    echo "Created taskmaster.db"
+fi
+
+docker compose down
+docker build -f Dockerfile.prod -t tasktracker:latest .
+docker compose up -d
+
+echo "Done. Running at http://localhost:8080"
